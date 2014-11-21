efc03ba8bae95ef89eb830bc7550bd1a1cfaf538
spiurao <spiurao@gmail.com>
2014-11-21 10:28:09 +0100

    Test souris

--- Test.py
+++ Test.py
@@ -1,1 +1,16 @@
 #test de pygame.mouse
+
+import pygame.display
+
+def recupCoor():
+	(x, y) = pygame.mouse.get_pos()
+	print("Position de la souris :"+ str(x) +"," + str(y))
+
+def afficheEcran(fen):
+	pygame.display.init()
+	fen.fill((255, 255, 255))
+
+fenetre = pygame.display.set_mode((800, 600))
+afficheEcran(fenetre)
+while True:
+	recupCoor()
