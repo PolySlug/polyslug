e2b8c892e4551cbc77d8861bd35cb0482f68bdc2
heltarion <clement49100@gmail.com>
2014-11-21 11:38:59 +0100

    boucle ecouteTouche marche

--- deplacement/essaiDeplacement.py
+++ deplacement/essaiDeplacement.py
@@ -1,42 +1,75 @@
 #test de deplacement
 import pygame
+import platform
+
+
+systeme = platform.system()
 
 def ecouteTouche():
     done = False
+    
 
+        
     while not done:
      
         for event in pygame.event.get():
-     
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    print ('q')
-                elif event.key == pygame.K_d:
-                    print ('d')
-                elif event.key == pygame.K_z:
-                    print ('z')
-                elif event.key == pygame.K_s:
-                    print ('s')
-                elif event.key == pygame.K_ESCAPE:
-                    print ('echap')
-                elif event.key == pygame.K_SPACE:
-                    print ('espace')
-     
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_q:
-                    print ('q')
-                elif event.key == pygame.K_d:
-                    print ('d')
-                elif event.key == pygame.K_z:
-                    print ('z')
-                elif event.key == pygame.K_s:
-                    print ('s')
-                elif event.key == pygame.K_ESCAPE:
-                    print ('echap')
-                elif event.key == pygame.K_SPACE:
-                    print ('espace')
+            if systeme == 'Windows':
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        print ('q')
+                    elif event.key == pygame.K_w:
+                        print ('z')
+                    elif event.key == pygame.K_s:
+                        print ('s')
+                    elif event.key == pygame.K_d:
+                        print ('d' )
+                    elif event.key == pygame.K_ESCAPE:
+                        print ('echap')
+                    elif event.key == pygame.K_SPACE:
+                        print ('espace')
+         
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        print ('q')
+                    elif event.key == pygame.K_w:
+                        print ('z')
+                    elif event.key == pygame.K_s:
+                        print ('s')
+                    elif event.key == pygame.K_d:
+                        print ('d' )
+                    elif event.key == pygame.K_ESCAPE:
+                        print ('echap')
+                    elif event.key == pygame.K_SPACE:
+                        print ('espace')
+            else:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        print ('q')
+                    elif event.key == pygame.K_d:
+                        print ('d')
+                    elif event.key == pygame.K_z:
+                        print ('z')
+                    elif event.key == pygame.K_s:
+                        print ('s')
+                    elif event.key == pygame.K_ESCAPE:
+                        print ('echap')
+                    elif event.key == pygame.K_SPACE:
+                        print ('espace')
+                        
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_q:
+                        print ('q')
+                    elif event.key == pygame.K_d:
+                        print ('d')
+                    elif event.key == pygame.K_z:
+                        print ('z')
+                    elif event.key == pygame.K_s:
+                        print ('s')
+                    elif event.key == pygame.K_ESCAPE:
+                        print ('echap')
+                    elif event.key == pygame.K_SPACE:
+                        print ('espace')
          
 pygame.init()         
 fenetre=pygame.display.set_mode((100, 100))
-ecouteTouche()                             
-    
+ecouteTouche()
