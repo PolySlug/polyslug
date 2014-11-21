ebdcac2471c21bbc75d76ab5137f5f96a79eaf6b
spiurao <spiurao@gmail.com>
2014-11-21 11:42:38 +0100

    En cours de modif : class

--- Test.py
+++ Test.py
@@ -1,13 +1,24 @@
 #test de pygame.mouse
+import pygame.display
+from pygame.locals import *
 
+class Viseur():
 
-def afficheCurseur(fen):
-	pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0], recupCoor()[1]-20), 2)
-	pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0], recupCoor()[1]+20), 2)
-	pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0]-20, recupCoor()[1]), 2)
-	pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0]+20, recupCoor()[1]), 2)
-	pygame.draw.circle(fen, (255, 50, 50), (recupCoor()[0]+1, recupCoor()[1]+1), 4)
-	pygame.display.flip()
+	def recupCoor(self):
+		#Récupère la position (x, y) de la souris
+		return pygame.mouse.get_pos()
+		
+
+	def draw(self, fen, x, y):
+		#Dessine 4 traits de longueur l=20px
+		pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0], recupCoor()[1]-20), 2)
+		pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0], recupCoor()[1]+20), 2)
+		pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0]-20, recupCoor()[1]), 2)
+		pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0]+20, recupCoor()[1]), 2)
+		#Dessine un cercle rouge de rayon r=4px
+		pygame.draw.circle(fen, (255, 50, 50), (recupCoor()[0]+1, recupCoor()[1]+1), 4)
+		pygame.display.flip()
+
 
 def afficheEcran(fen):
 	pygame.display.init()
@@ -18,23 +29,5 @@ fenetre = pygame.display.set_mode((800, 600))
 pygame.mouse.set_visible(False)
 while True:
 	pygame.time.wait(60)
-	afficheEcran(fenetre)
-	afficheCurseur(fenetre)
+	Viseur.draw(fenetre)
 
-import pygame.display
-from pygame.locals import *
-
-class Viseur():
-
-	def recupCoor():
-		pygame.event.get()
-		return pygame.mouse.get_pos()
-		
-
-	def draw(self, fenetre, x, y):
-		pygame.draw.line(fen, (0, 0, 0), (x, y), (x, y-20), 2)
-		pygame.draw.line(fen, (0, 0, 0), (x, y), (x, y+20), 2)
-		pygame.draw.line(fen, (0, 0, 0), (x, y), (x-20, y), 2)
-		pygame.draw.line(fen, (0, 0, 0), (recupCoor()[0], recupCoor()[1]), (recupCoor()[0]+20, recupCoor()[1]), 2)
-		pygame.draw.circle(fen, (255, 50, 50), (recupCoor()[0]+1, recupCoor()[1]+1), 4)
-		pygame.display.flip()
