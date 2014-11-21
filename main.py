856aec4ba1a7aa3862f2d254eb3411be32b13389
Guillaume Wuip <guillaume@wuips.com>
2014-11-21 12:16:42 +0100

    Ajout doc main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #fichier main
+
+'''
+- init pygame et création fenetre
+- charger les ecrans
+- variable "etat"
+- boucle while
+    - ecoute de l'évènement quit
+    - selon etat, appeller les fonction boucles() de chaques ecrans
+'''
