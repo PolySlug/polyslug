3f4ff9a3cbe44183e1c3837ab266c4a2f363a28f
Guillaume Wuip <guillaume@wuips.com>
2014-11-28 09:49:03 +0100

    Classe Entite

--- entitees/entitees.py
+++ entitees/entitees.py
@@ -1,3 +1,20 @@
-''' entitees.py 
-Mettre ici les classes des entitees
-'''
+
+import pygame
+
+class Entite(pygame.sprite.Srite) :
+
+    self.vie = 100
+
+    def __init__(self, position) :
+        self.position = position
+
+    def update(self) :
+        pass
+
+    def draw(self, calque):
+        pass
+
+    def blessure(self, vie):
+        self.vie -= vie
+        return self.vie
+
