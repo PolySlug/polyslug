89a65c75ade2629e2b67a9887e27a177617dd39e
Eilwenn <lj.gaelle@gmail.com>
2014-11-28 11:23:08 +0100

    add armes
    ajout armes

---
+++ armes/armes.py
@@ -0,0 +1,9 @@
+class Arme(object) :
+
+	def __init__(self): 
+		'''Caracteristiques de la classe arme'''
+		pass
+
+	def tirer(self, position, vecteur) :
+		'''cree et envoie un projectile dans une direction'''
+		pass
