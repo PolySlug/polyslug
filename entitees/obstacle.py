521c1c2384338f80dc84be792196a869d3c227d2
Guillaume Wuip <guillaume@wuips.com>
2014-11-28 09:56:23 +0100

    stub obstacles

--- entitees/obstacle.py
+++ entitees/obstacle.py
@@ -1,2 +1,11 @@
-''' obstacle.py
-fichier relatif a la classe obstacles uniquement'''
+
+from entitee import Entite
+#from sprites import Sprites
+
+class Obstacle(Entite) :
+
+    self.vie = 10
+    #self.sprites = Sprites
+
+    def draw(self, calque) :
+        calque.blit(self.sprite.sprite((0,0), (10,10)) , self.position)
