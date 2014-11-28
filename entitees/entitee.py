aeffb1efce1116cd61127064ecec83c8badbcdca
Guillaume Wuip <guillaume@wuips.com>
2014-11-28 21:35:12 +0100

    Ajout commentaires

--- entitees/entitee.py
+++ entitees/entitee.py
@@ -1,19 +1,51 @@
+#-*- coding: utf-8 -*-
 
 import pygame
 
-class Entite(pygame.sprite.Srite) :
+'''
+Entite
 
-    self.vie = 100
+'''
+class Entite(pygame.sprite.Sprite) :
 
+    '''
+    vie
+
+    La vie de l'entitée
+    '''
+    vie = 100
+
+    '''
+    rect
+
+    Position et surface occupée par l'entitée
+    @type {pygame.Rect} Les deux premiers int sont pour la position x, y
+                        Les deux derniers sont pour width, height
+    '''
+    rect = pygame.Rect(0, 0, 10, 10)
+
+    '''
+    @constructor
+
+    @param {Tupple} position    Position de l'entitée
+    '''
     def __init__(self, position) :
-        self.position = position
+        pygame.sprite.Sprite.__init__(self)
+
+        #Prise en compte de la position
+        x, y = position
+        self.rect[0] = x
+        self.rect[0] = y
 
     def update(self) :
         pass
 
-    def draw(self, calque):
-        pass
+    '''
+    blessure
 
+    Fonction par défaut. Modifiée dans certains cas
+    @param {Int}    vie     La quantité de vie à enlever
+    '''
     def blessure(self, vie):
         self.vie -= vie
         return self.vie
