cd7a62e31701a10daa5ce5ae2974421db7bb4d61
Guillaume Wuip <guillaume@wuips.com>
2014-11-30 19:55:13 +0100

    Ecoute q et d et mouvement calque

--- jeu.py
+++ jeu.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 
 import pygame
+import platform
 
 '''
 gestionJeu
@@ -15,27 +16,57 @@ gestionJeu
 def gestionJeu(fenetre, niveau):
 
     #Création des groupes de sprites
-    groupeMurs      = creationGroupe(murs)
-    groupeObstacles = creationGroupe(obstacles)
-    groupeEnnemis   = creationGroupe(ennemis)
+    groupeMurs      = creationGroupe(niveau['murs'])
+    groupeObstacles = creationGroupe(niveau['obstacles'])
+    groupeEnnemis   = creationGroupe(niveau['ennemis'])
 
-    groupeJeu       = creationGroupe(murs + obstacles + ennemis + joueur)
+    groupeJeu       = creationGroupe(niveau['murs'] + niveau['obstacles'] + \
+            niveau['ennemis'] + niveau['joueur'])
 
     #Création d'un calque
     calque = pygame.Surface((1000, fenetre.get_height()))
+    decalageX = 0
 
     done = False
 
     #Init du temps
     clock = pygame.time.Clock()
 
+    #On récupère le système (Win/Mac/etc) pour les touches
+    systeme = platform.system()
+
     while not done :
 
+        #Écoute des touches clavier
+
+        for event in pygame.event.get() :
+
+            if systeme == "Windows" : #pygame sur windows est en Qwerty ...
+                if event.type == pygame.KEYDOWN :
+                    if event.key == pygame.K_a :
+                        decalageX -= 1
+                    if event.key == pygame.K_d :
+                        decalageX += 1
+
+            else :
+                if event.type == pygame.KEYDOWN :
+                    if event.key == pygame.K_q :
+                        decalageX -= 1
+                    if event.key == pygame.K_d :
+                        decalageX += 1
+
+        #Contrôle
+        decalageX = decalageX if decalageX > -1 else 0
+
+        #On update tout le petit monde
         groupeJeu.update()
+
+        #On dessine dans le calque
         groupeJeu.draw(calque)
 
+        #On insère le calque dans le fenêtre en fonction de decalageX
         fenetre.fill((0, 0, 0))
-        fenetre.blit(calque, (0,0))
+        fenetre.blit(calque, (-decalageX * 3, 0))
         pygame.display.flip()
 
         clock.tick(60)
