9d6bd6ff5ac91e09350c94f92ad4216137f1b641
spiurao <spiurao@gmail.com>
2014-12-05 09:31:37 +0100

    ecouteSouris finie + projectiles a tester

--- lib/viseur.py
+++ lib/viseur.py
@@ -1,12 +1,9 @@
 #test de pygame.mouse
 import pygame.display
 from pygame.locals import *
+from ecouteSouris import ecouteSouris
 
 class Viseur():
-
-	def recupCoor(self):
-		#Recupere la position (x, y) de la souris
-		return pygame.mouse.get_pos()
 		
 	'''
 	draw
@@ -17,28 +14,10 @@ class Viseur():
 	'''
 	def draw(self, fen):
 		#Dessine 4 traits de longueur l=20px
-		pygame.draw.line(fen, (0, 0, 0), (self.recupCoor()[0], self.recupCoor()[1]), (self.recupCoor()[0], self.recupCoor()[1]-20), 2)
-		pygame.draw.line(fen, (0, 0, 0), (self.recupCoor()[0], self.recupCoor()[1]), (self.recupCoor()[0], self.recupCoor()[1]+20), 2)
-		pygame.draw.line(fen, (0, 0, 0), (self.recupCoor()[0], self.recupCoor()[1]), (self.recupCoor()[0]-20, self.recupCoor()[1]), 2)
-		pygame.draw.line(fen, (0, 0, 0), (self.recupCoor()[0], self.recupCoor()[1]), (self.recupCoor()[0]+20, self.recupCoor()[1]), 2)
+		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0], ecouteSouris()[1]-20), 2)
+		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0], ecouteSouris()[1]+20), 2)
+		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0]-20, ecouteSouris()()[1]), 2)
+		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0]+20, ecouteSouris()[1]), 2)
 		#Dessine un cercle rouge de rayon r=4px
-		pygame.draw.circle(fen, (255, 50, 50), (self.recupCoor()[0]+1, self.recupCoor()[1]+1), 4)
+		pygame.draw.circle(fen, (255, 50, 50), (ecouteSouris()[0]+1, ecouteSouris()[1]+1), 4)
 		pygame.display.flip()
-
-
-def afficheEcran(fen):
-	pygame.display.init()
-	fen.fill((255, 255, 255))
-	pygame.display.flip()
-
-fenetre = pygame.display.set_mode((800, 600))
-pygame.mouse.set_visible(False)
-
-viseur = Viseur()
-
-while True:
-	pygame.event.get()
-	pygame.time.wait(30)
-	afficheEcran(fenetre)
-	viseur.draw(fenetre)
-
--- projectiles/projectile.py
+++ projectiles/projectile.py
@@ -1,12 +1,12 @@
 #test de projectiles
-import pygame.display
+import pygame
 
 class Projectile(pygame.sprite.Sprite):
 
 	self.rect =  (0, 0)
         self.image = None
 	self.sprite = None
-	self.dommage = 0
+	self.dommage = 10
 
 	'''
 	__init__
@@ -16,15 +16,25 @@ class Projectile(pygame.sprite.Sprite):
 	@param {tuple} position & vecteur
 	'''
 	def __init__(self, position, vecteur):
+
+ 		pygame.sprite.Sprite.__init__(self)
+
+        	#On récupère le rectangle de l'image
+	        self.rect = self.image.get_rect()
 		
-		self.position = (x, y)
-		self.image = pygame.draw.circle(fen, (120,120,0), x, y, 5)
-		self.vitesse = (a, b)
+		self.rect.x, self.rect.y = position
+
+		vx, vy = vecteur
+
+		#Calcul norme
+		norme = math.sqrt(vx*vx + vy*vy)
+
+		self.vitesse = (vx / norme, vy / norme)
 
 	'''
 	Update la position du projectile grace au tuple vitesse
 	'''
-	def update(self):
-		self.rect = (a+x, y+b)		
+	def update(self, *args):
+		pass
 		# a la fin de update, changer self.image à partir de self.sprite
 		# et si besoin, changer self.rect
