14290ad505544797b26855ca024e6dea18b07616
Eilwenn <lj.gaelle@gmail.com>
2014-12-05 11:41:37 +0100

    Fix

--- projectiles/projectile.py
+++ projectiles/projectile.py
@@ -1,40 +1,42 @@
+#-*- coding:utf-8-*-
+
 #test de projectiles
 import pygame
 
 class Projectile(pygame.sprite.Sprite):
 
-	self.rect =  (0, 0)
-    self.image = None
-	self.sprite = None
-	self.dommage = 10
+    rect =  (0, 0)
+    image = None
+    sprite = None
+    dommage = 10
 
-	'''
-	__init__
+    '''
+    __init__
 
-	Initialise les position, vitesse et sprite du projectile
+    Initialise les position, vitesse et sprite du projectile
 
-	@param {tuple} position & vecteur
-	'''
-	def __init__(self, position, vecteur):
+    @param {tuple} position & vecteur
+    '''
+    def __init__(self, position, vecteur):
 
- 		pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
 
-        	#On récupère le rectangle de l'image
-	        self.rect = self.image.get_rect()
-		
-		self.rect.x, self.rect.y = position
+        #On récupère le rectangle de l'image
+        self.rect = self.image.get_rect()
+        
+        self.rect.x, self.rect.y = position
 
-		vx, vy = vecteur
+        vx, vy = vecteur
 
-		#Calcul norme
-		norme = math.sqrt(vx*vx + vy*vy)
+        #Calcul norme
+        norme = math.sqrt(vx*vx + vy*vy)
 
-		self.vitesse = (vx / norme, vy / norme)
+        self.vitesse = (vx / norme, vy / norme)
 
-	'''
-	Update la position du projectile grace au tuple vitesse
-	'''
-	def update(self, *args):
-		pass
-		# a la fin de update, changer self.image à partir de self.sprite
-		# et si besoin, changer self.rect
+    '''
+    Update la position du projectile grace au tuple vitesse
+    '''
+    def update(self, *args):
+        pass
+        # a la fin de update, changer self.image à partir de self.sprite
+        # et si besoin, changer self.rect
