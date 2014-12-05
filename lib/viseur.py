fa310c9dc0e93cdf2d442bd6b1633248ea617108
Guillaume Wuip <guillaume@wuips.com>
2014-12-05 09:55:45 +0100

    Fix viseur

--- lib/viseur.py
+++ lib/viseur.py
@@ -12,11 +12,14 @@ class Viseur():
 	@param {Surface} fen
 	'''
 	def draw(self, fen):
-		#Dessine 4 traits de longueur l=20px
-		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0], ecouteSouris()[1]-20), 2)
-		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0], ecouteSouris()[1]+20), 2)
-		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0]-20, ecouteSouris()()[1]), 2)
-		pygame.draw.line(fen, (0, 0, 0), (ecouteSouris()[0], ecouteSouris()[1]), (ecouteSouris()[0]+20, ecouteSouris()[1]), 2)
-		#Dessine un cercle rouge de rayon r=4px
-		pygame.draw.circle(fen, (255, 50, 50), (ecouteSouris()[0]+1, ecouteSouris()[1]+1), 4)
-		pygame.display.flip()
+
+            position = ecouteSouris()
+
+            #Dessine 4 traits de longueur l=20px
+            pygame.draw.line(fen, (0, 0, 0), (position[0],position[1]), (position[0],position[1]-20), 2)
+            pygame.draw.line(fen, (0, 0, 0), (position[0],position[1]), (position[0],position[1]+20), 2)
+            pygame.draw.line(fen, (0, 0, 0), (position[0],position[1]), (position[0]-20,position[1]), 2)
+            pygame.draw.line(fen, (0, 0, 0), (position[0],position[1]), (position[0]+20,position[1]), 2)
+            #Dessine un cercle rouge de rayon r=4px
+            pygame.draw.circle(fen, (255, 50, 50), (position[0]+1, position[1]+1), 4)
+
