ff59deddce2f21e8a7fc247134962dc2c82e3a4d
Guillaume Wuip <guillaume@wuips.com>
2014-12-07 19:45:43 +0100

    Ennemi2

---
+++ entites/ennemi2.py
@@ -0,0 +1,43 @@
+#-*- coding: utf-8 -*-
+
+from ennemi      import Ennemi
+from lib.sprites import Sprites
+from armes.arme2 import Arme2
+
+class Ennemi2(Ennemi):
+
+    sprites = Sprites('img/p3_spritesheet.png')
+
+    arme = Arme2()
+
+    vie = 20
+
+    '''
+    construireSprites
+    '''
+    def contruireSprites(self) :
+
+        #images mouvement
+        images = [
+            [(0, 0), (72, 97)],
+            [(73, 0), (72, 97)],
+            [(146, 0), (72, 97)],
+            [(0, 98), (72, 97)],
+            [(73, 98), (72, 97)],
+            [(146, 98), (72, 97)],
+            [(219, 0), (72, 97)],
+            [(284, 0), (72, 97)],
+            [(219, 98), (72, 97)],
+            [(365, 0), (72, 97)],
+            [(292, 98), (72, 97)],
+        ]
+
+        self.framesDroites = self.sprites.sprites(images)
+        self.framesGauches = self.sprites.sprites(images, flipX = True)
+
+        self.image = self.imageRepos = self.sprites.sprite((0, 196), (66, 92))
+
+        self.imageAccroupi = [
+            self.sprites.sprite((365, 98), (69, 71)),
+            self.sprites.sprite((365, 98), (69, 71), flipX = True)
+        ]
--- niveaux/niveau1.py
+++ niveaux/niveau1.py
@@ -3,6 +3,7 @@
 from entites.mur           import Mur
 from entites.obstacleTest  import ObstacleTest
 from entites.ennemi1       import Ennemi1
+from entites.ennemi2       import Ennemi2
 from entites.joueur        import Joueur
 
 '''
@@ -13,7 +14,7 @@ murs = [Mur((0, 400)), Mur((71, 400)), Mur((400, 550))]
 
 obstacles = [ObstacleTest((0,0)), ObstacleTest((100, 0))]
 
-ennemis = [Ennemi1((1000, 200))]
+ennemis = [Ennemi1((1000, 200)), Ennemi2((1200, 200))]
 
 joueur = Joueur((0, 200))
 
