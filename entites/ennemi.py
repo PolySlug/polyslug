1ee37b4b25ea3a1a5985061e8efb9d77d53f504d
Guillaume Wuip <guillaume@wuips.com>
2014-12-07 19:41:10 +0100

    Ajout doc

--- entites/ennemi.py
+++ entites/ennemi.py
@@ -6,7 +6,7 @@ from personnage import Personnage
 
 class Ennemi(Personnage):
 
-    multiCourse = 3/2     #vitesse de deplacementX (marcher/courir)
+    multiCourse = 3/2     #va 2 fois moins vite que le joueur
 
     def __init__(self, position) :
 
@@ -15,20 +15,48 @@ class Ennemi(Personnage):
 
         super(Ennemi, self).__init__(position)
 
+    '''
+    ia_directionJoueur
+
+    @param  {Personnage} joueur
+    @return {tuple}             Le vecteur distance ennemi/joueur
+    '''
     def ia_directionJoueur(self, joueur) :
         x, y   = self.position()
         jx, jy = joueur.position()
 
         return (jx-x, jy-y)
 
+    '''
+    ia_distanceJoueur
+
+    @param  {Personnage}    joueur
+    @return {int}                   La distance ennemi/joueur en px
+    '''
     def ia_distanceJoueur(self, joueur):
         vect = self.ia_directionJoueur(joueur)
 
         return math.sqrt( vect[0] * vect[0] + vect[1] * vect[1] )
 
+    '''
+    ia_joueurEnVue
+
+    @param  {Personnage}    joueur
+    @param  {int}           vision  Champ de vision de l'ennemi = distance max de px
+    @return {Bool}                  True si le joueur est dans le champ de vision de l'ennemi
+    '''
     def ia_joueurEnVue(self, joueur, vision) :
         return self.ia_distanceJoueur(joueur) < vision
 
+    '''
+    ia
+
+    La partie autonome de l'ennemi.
+    Tente de tuer le joueur si il est dans le champ de vision
+    Sinon, reste à sa place
+
+    @param  {dic}   niveau  l'état du niveau
+    '''
     def ia(self, niveau) :
 
         joueur              = niveau['joueur']
