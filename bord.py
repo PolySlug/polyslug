62df778cb0f4b2d28ca7219dff2ec35ed939d724
Guillaume Wuip <guillaume@wuips.com>
2014-12-08 08:24:58 +0100

    Passage spaces + ajout doc

--- bord.py
+++ bord.py
@@ -1,16 +1,28 @@
 import pygame
 
+'''
+Bord
+
+Un simple rectangle
+Sert à gérer les sorties de fenêtre (killer les projectiles par ex)
+'''
 class Bord(pygame.sprite.Sprite):
-        
-        def __init__(self, position, taille):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.Surface(taille)
-            self.image.fill((255, 0, 0))
-            self.rect = self.image.get_rect()
-            
-            self.position = position
-            self.rect.x, self.rect.y = position
-            
-        def update(self, decalageX):
-            self.rect.x = self.position[0] - decalageX
-        
+
+    def __init__(self, position, taille):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface(taille)
+        self.image.fill((255, 0, 0))
+
+        self.rect = self.image.get_rect()
+
+        self.position = position
+        self.rect.x, self.rect.y = position
+
+    '''
+    update
+
+    Le bord doit suivre le scroll du niveau
+    '''
+    def update(self, decalageX):
+        self.rect.x = self.position[0] - decalageX
