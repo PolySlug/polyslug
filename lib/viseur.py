8e31bc2ef2f751d9966e74b51838bd4d7dca4a07
Eilwenn <lj.gaelle@gmail.com>
2014-12-09 22:05:11 +0100

    resolution conflit viseur
    Suppression lignes " head / >>>> truc"

--- lib/viseur.py
+++ lib/viseur.py
@@ -22,9 +22,4 @@ class Viseur():
             pygame.draw.line(fen, (0, 0, 0), (position[0],position[1]), (position[0]+20,position[1]), 2)
             #Dessine un cercle rouge de rayon r=4px
             pygame.draw.circle(fen, (255, 50, 50), (position[0]+1, position[1]+1), 4)
-<<<<<<< HEAD
-=======
 
-
-			
->>>>>>> origin/clement/plateforme
