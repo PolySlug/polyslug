199368531899537ba478386e35915eff2c4598b7
Guillaume Wuip <guillaume@wuips.com>
2014-12-11 08:36:07 +0100

    MAJ entités Mur et Plateformes

--- entites/plateforme.py
+++ entites/plateforme.py
@@ -1,18 +1,9 @@
 #-*- coding: utf-8 -*-
 
-from entite      import Entite
+from mur         import Mur
 from lib.sprites import Sprites
 
-class Plateforme (Entite):
+class Plateforme (Mur):
 
-    sprites = Sprites('img/bridge.png')
-    image   = sprites.sprite((0, 50), (72,30))
     plateforme = True
 
-    '''
-    blessure
-
-    Un mur ne peut pas mourir
-    '''
-    def blessure(self) :
-        return 1
