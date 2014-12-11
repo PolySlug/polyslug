c788cd4c017be29fffed684beb199fef995470dd
Guillaume Wuip <guillaume@wuips.com>
2014-12-11 19:48:34 +0100

    Fin création des portails

--- portail.py
+++ portail.py
@@ -4,15 +4,14 @@ from lib.sprites import Sprites
 
 class Portail(pygame.sprite.Sprite) :
 
-    sprite = Sprites("img/tiles.png")
-    image = self.sprite.sprite((280, 280), (70, 70))
+    sprite = Sprites("img/tiles_spritesheet.png")
+    image  = sprite.sprite((280, 280), (70, 70))
 
     def __init__(self, position, suivant) :
 
         pygame.sprite.Sprite.__init__(self)
 
         self.rect = self.image.get_rect()
-
         self.rect.x, self.rect.y = position
 
         self.suivant = suivant
