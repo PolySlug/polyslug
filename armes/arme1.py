8f49f12a8d1faa2277c80cf60a5acec7d758db34
spiurao <spiurao@gmail.com>
2014-12-12 09:48:23 +0100

    correction du commentaire ascii caca

--- armes/arme1.py
+++ armes/arme1.py
@@ -22,7 +22,7 @@ class Arme1(Arme) :
         if pygame.time.get_ticks() - self.dernierTir > self.delai :
 
             super(Arme1, self).tirer(position, vecteur)
-            return [ProjectileDroitRouge((position[0]+30*((2*self.angle)/(1+self.angle*self.angle)), position[1]+20+30*((1-self.angle*self.angle)/(1+self.angle*self.angle))), vecteur)] #Le tuple position est modifie afin de parametrer un cercle de rayon r=30px selon l'angle car on a besoin de faire sortir le projectile du canon de l'arme et non de la positionMain. COmme l'arme effectue une rotation sur elle même et qu'elle est environ de rayon 30px, on a besoin d'utiliser l'expression d'un cercle
+            return [ProjectileDroitRouge((position[0]+30*((2*self.angle)/(1+self.angle*self.angle)), position[1]+20+30*((1-self.angle*self.angle)/(1+self.angle*self.angle))), vecteur)] #Le tuple position est modifie afin de parametrer un cercle de rayon r=30px selon l'angle car on a besoin de faire sortir le projectile du canon de l'arme et non de la positionMain. Comme l'arme effectue une rotation sur elle meme et qu'elle est environ de rayon 30px, on a besoin d'utiliser l'expression d'un cercle
 
         else :
             return []
