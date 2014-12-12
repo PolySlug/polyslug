ffc102923f14e99532fe94dec0ab913cc242989f
Guillaume Wuip <guillaume@wuips.com>
2014-12-12 15:40:08 +0100

    Vitesse projectiles plus grande

--- projectiles/projectileSinus.py
+++ projectiles/projectileSinus.py
@@ -14,11 +14,11 @@ class ProjectileSinus(Projectile):
     def __init__(self, position, vecteur):
         super(ProjectileSinus, self).__init__(position, vecteur)
         self.posx, self.posy = (float(position[0]), float(position[1]))
-        
+
     def update(self, *args) :
-      
+
         self.posx += self.vitesse[0] * self.multi
         self.posy += self.vitesse[1] * self.multi + 2*math.sin(0.005*pygame.time.get_ticks()) #depend du temps
-	
+
         self.rect.x = int(self.posx)
         self.rect.y = int(self.posy)
